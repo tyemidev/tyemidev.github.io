66443b40926fc067697d2d079c2fccb28679ac36
tyemidev <TyemiDev@gamil.com>
2020-07-28 10:21:37 -0400

    listgen test

--- tyemi.js
+++ tyemi.js
@@ -6,21 +6,27 @@
 
 function listgen(ulid, display, switcheroffid, switcheronid){
     //declare variables
-    var ul, offid, onid, li, button, buttontext;
+    var ul, li, button;
 
     //get element input
     ul = document.getElementById(ulid);
-    offid = document.getElementById(switcheroffid);
-    onid = document.getElementById(switcheronid);
 
-    //create, modify and append elements
+    //create elements
     li = document.createElement('li');
     button = document.createElement('button');
-    button.setAttribute("onclick", "switcher('"+ offid + "', '"+ onid +"');");
-    buttontext = document.createTextNode(display);
-    button.appendChild(buttontext);
+
+    //content
+    button.textContent = display;
+
+    //attributes
+    button.setAttribute('onclick', 'test'+' nunu')
+
+    //append
     li.appendChild(button);
     ul.appendChild(li);
+
+    //switcher
+    switcher(switcheroffid, switcheronid);
 }
 
 function switcher(close, open){
