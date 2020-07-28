ffff82ac3686896ab3beba53f7825f01286d578f
tyemidev <TyemiDev@gamil.com>
2020-07-28 12:14:08 -0400

    litgen(); works now. Started work on appgen();

--- tyemi.js
+++ tyemi.js
@@ -26,7 +26,32 @@ function listgen(ulid, display, switcheroffid, switcheronid){
     ul.appendChild(li);
 }
 
+function appgen(id, name, description, link, offline){
+    //declare variables
+    var h1, br1, h3, br2, br3, button1, br4, button2;
+
+    //create elements
+    h1 = document.createElement('h1');
+    br1 = document.createElement('br/');
+    h3 = document.createElement('h3');
+    br2 = document.createElement('br/');
+    br3 = document.createElement('br/');
+    button1 = document.createElement('button');
+    br4 = document.createElement('br/');
+    button2 = document.createElement('button');
+}
+
 function switcher(close, open){
     document.getElementById(close).style.display = 'none';
     document.getElementById(open).style.display = 'block';
+}
+
+function window(method, url){
+    if(method == 'location'){
+        window.location = url;
+    }else if(method == 'location_href'){
+        window.location.href = url;
+    }else if(method == 'open'){
+        window.open(url);
+    }
 }
