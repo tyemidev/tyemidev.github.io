d6f99a3b36eef23248c6938e1acd6cc21e505406
tyemidev <TyemiDev@gamil.com>
2020-07-29 11:50:10 -0400

    appgen complete.

--- tyemi.js
+++ tyemi.js
@@ -4,6 +4,8 @@
  *                    *
  **********************/
 
+
+// Generation functions
 function listgen(ulid, display, switcheroffid, switcheronid){
     //declare variables
     var ul, li, button;
@@ -28,9 +30,10 @@ function listgen(ulid, display, switcheroffid, switcheronid){
 
 function appgen(id, name, description, link, offline){
     //declare variables
-    var h1, br1, h3, br2, br3, button1, br4, button2;
+    var div, h1, br1, h3, br2, br3, button1, br4, button2;
 
     //create elements
+    div = document.getElementById(id);
     h1 = document.createElement('h1');
     br1 = document.createElement('br/');
     h3 = document.createElement('h3');
@@ -38,9 +41,46 @@ function appgen(id, name, description, link, offline){
     br3 = document.createElement('br/');
     button1 = document.createElement('button');
     br4 = document.createElement('br/');
+
+    if(offline){
     button2 = document.createElement('button');
+    }
+
+    //content
+    h1.textContent = name;
+    h3.textContent = description;
+    button1.textContent = 'Install';
+
+    if(offline){
+    button2.textContent = 'Install (offline)';
+    }
+
+    //attributes
+    h1.setAttribute('style', 'font-size:large;');
+    h3.setAttribute('style', 'font-size:large;');
+    button1.setAttribute('style', 'font-size:2vw; width:4vw;');
+    button1.setAttribute('onclick', 'window("open", "'+link+'");');
+
+    if(offline){
+    button2.setAttribute('style', 'font-size:2vw; width:4vw;');
+    button2.setAttribute('onclick', 'window("open", "'+offline+'");');
+    }
+
+    //append
+    div.appendChild(h1);
+    div.appendChild(br1);
+    div.appendChild(h3);
+    div.appendChild(br2);
+    div.appendChild(br3);
+    div.appendChild(button1);
+    div.appendChild(br4);
+    if(offline){
+    div.appendChild(button2);
+    }
 }
 
+///////////////////////////////////////////////////////
+// Viewing functions
 function switcher(close, open){
     document.getElementById(close).style.display = 'none';
     document.getElementById(open).style.display = 'block';
