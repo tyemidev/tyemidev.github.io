1e3dc603ed1d9300d4e594f87c63aa4006dae5ee
tyemidev <TyemiDev@gamil.com>
2020-11-01 09:00:47 -0500

    progress on CUB3

--- tyemicube/cube/resources/apputil.js
+++ tyemicube/cube/resources/apputil.js
@@ -6,6 +6,14 @@ document.onload = function(){
     var hashParams = window.location.hash.substr(1).split('&'); // substr(1) to remove the `#`
     for(var i = 0; i < hashParams.length; i++){
         var p = hashParams[i].split('=');
-    document.getElementById(p[0]).value = decodeURIComponent(p[1]);;
+        document.getElementById(p[0]).value = decodeURIComponent(p[1]);;
+    }
+}
+
+function attempt(){
+    var hashParams = window.location.hash.substr(1).split('&'); // substr(1) to remove the `#`
+    for(var i = 0; i < hashParams.length; i++){
+        var p = hashParams[i].split('=');
+        document.getElementById(p[0]).setAttribute("value", decodeURIComponent(p[1]));
     }
 }
